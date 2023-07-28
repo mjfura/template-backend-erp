31df532706a863e3aa9f9f2d918e2bc63bf57af3
Marco Fura <mfura@kataricorp.com>
2023-07-27 20:56:10 -0500

    feat: add new endpoint for auth

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import cors from "cors"
 import {config} from "dotenv"
 import userRoutes from "./app/Users/infrastructure/routes"
 import empresaRoutes from "./app/Empresas/infrastructure/routes"
+import authRoutes from "./app/Auth/infraestructure/routes"
 config()
 const app=express()
 app.use(morgan("dev"))
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/empresas",empresaRoutes)
-app.use('api/users',userRoutes)
+app.use('/api/users',userRoutes)
+app.use('/api/auth',authRoutes)
 
 export default app
