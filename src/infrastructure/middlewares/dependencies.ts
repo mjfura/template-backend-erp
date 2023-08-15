a46bc0ac07fa2a5b155b7c72ce4cd83ba8bd870d
Marco Fura <mfura@kataricorp.com>
2023-08-14 20:09:23 -0500

    feat: add new token repository for middleware

--- src/infrastructure/middlewares/dependencies.ts
+++ src/infrastructure/middlewares/dependencies.ts
@@ -1,6 +1,8 @@
 import { MiddlewareUseCase } from "../../application/middlewares";
 import { NodeResponserRepository } from "../responser/repository";
+import { JwtRepository } from "../token/repository";
 import { AppMiddleware } from "./index.middleware";
 const responserRepository=new NodeResponserRepository()
-const middlewareUseCase=new MiddlewareUseCase(responserRepository)
+const tokenRepository=new JwtRepository()
+const middlewareUseCase=new MiddlewareUseCase(responserRepository,tokenRepository)
 export const appMiddleware=new AppMiddleware(middlewareUseCase)
